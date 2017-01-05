549e8fac916846756bca0a3fd186625eb87df668
Arjun Rao <arjun210493@gmail.com>
2017-01-05 13:09:01 +0400

    Created a SetupTools Package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="genericbuilder",
+    version="0.1",
+    packages=['genericbuilder'],
+    author="Arjun Rao",
+    author_email="arjun210493@gmail.com",
+    description="This module provides the infrastructure to create custom (spike/rate) builders",
+    license="MIT",
+    keywords="Generic Builder builder generic",
+    provides=['genericbuilder']
+)
